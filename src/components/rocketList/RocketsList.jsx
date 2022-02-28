bba2e39d8b69886d340ff657c41482c1a2bcb280
jihaneh <jijih@hotmail.fr>
2022-02-28 16:46:22 +0100

    Fix linters

--- src/components/rocketList/RocketsList.jsx
+++ src/components/rocketList/RocketsList.jsx
@@ -2,24 +2,13 @@ import React from 'react';
 import Rocket from '../rocket/Rocket';
 
 const RokectsList = () => {
-
-    const keys = [
-        { id: 1},
-        { id: 2},
-        { id: 3},
-        { id: 4},
-    ]
-    const rockets = keys.map((rocket) => ( 
-        <li key={rocket.id}>
-            <Rocket />
-        </li>
-    ))
-
-    return (
-        <ul>
-            {rockets}
-        </ul>
-    );
-}
+  const keys = [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }];
+  const rockets = keys.map((rocket) => (
+    <li key={rocket.id}>
+      <Rocket />
+    </li>
+  ));
+  return <ul>{rockets}</ul>;
+};
 
 export default RokectsList;
