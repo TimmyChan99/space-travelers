f779b85bb3138c921455e2cff5d3ec0b0569f6a6
jihaneh <jijih@hotmail.fr>
2022-02-28 11:48:51 +0100

    Add Routes, Route in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/header/Header';
+import Profile from './components/profile/Profile';
 
 function App() {
   return (
     <>
       <Header />
       <div className="App" />
+      <Routes>
+        <Route path="my-profile" element={<Profile />} />
+      </Routes>
     </>
   );
 }
