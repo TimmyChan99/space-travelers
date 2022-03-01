94505f47d29fdd76f214411bbb6389ff603d3d2e
jihaneh <jijih@hotmail.fr>
2022-03-01 09:29:10 +0100

    Add button to join/leave mission and display status

--- src/components/missions/Mission.js
+++ src/components/missions/Mission.js
@@ -2,10 +2,43 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default function Mission({ mission }) {
+  const handleJoinMission = (id) => {
+    console.log(id);
+  };
+  const handleLeaveMission = (id) => {
+    console.log(id);
+  };
+
+  const status = mission.status ? (
+    <div className="active-mb"> Active Member</div>
+  ) : (
+    <div className="no-active-mb"> NOT A MEMBER</div>
+  );
+
+  const missionBt = !mission.status ? (
+    <button
+      type="button"
+      className="join-mission-bt"
+      onClick={() => handleJoinMission(mission.missionId)}
+    >
+      Join Mission
+    </button>
+  ) : (
+    <button
+      type="button"
+      className="leave-mission-bt"
+      onClick={() => handleLeaveMission(mission.missionId)}
+    >
+      Leave Mission
+    </button>
+  );
+
   return (
     <div className="mission-container">
       <div className="mission-name">{mission.missionName}</div>
       <div className="mission-description">{mission.description}</div>
+      {status}
+      {missionBt}
     </div>
   );
 }
