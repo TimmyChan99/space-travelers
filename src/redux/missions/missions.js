9b28a8f10244bcc3cabbe1659698fa6c224cc7e0
jihaneh <jijih@hotmail.fr>
2022-03-01 12:30:32 +0100

    Merge joinmission and leave mission

--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -2,7 +2,7 @@ import { getMissionsFromAPI } from '../../api/spacexdata';
 
 const INITIALIZE_MISSIONS = 'space-travelers/missions/INITIALIZE_MISSIONS';
 const GET_MISSIONS = 'space-travelers/missions/GET_MISSIONS';
-const JOIN_LEAVE_MISSION = 'space-travelers/missions/JOIN_MISSION';
+const TOGGLE_MISSION = 'space-travelers/missions/TOGGLE_MISSION';
 
 const initialState = {
   isMissionsStored: false,
@@ -26,13 +26,12 @@ export const getMissionsDispatcher = () => async (dispatch) => {
   dispatch(getMissions());
 };
 
-export const joinMission = (data) => ({
-  type: JOIN_LEAVE_MISSION,
+export const toggleMission = (data) => ({
+  type: TOGGLE_MISSION,
   payload: data,
 });
 
-export const joinMissionDispatcher = (id) => (dispatch) => dispatch(joinMission(id));
-export const leaveMissionDispatcher = (id) => (dispatch) => dispatch(joinMission(id));
+export const toggleMissionDispatcher = (id) => (dispatch) => dispatch(toggleMission(id));
 
 const missionsReducer = (state = initialState, action) => {
   switch (action.type) {
@@ -46,7 +45,7 @@ const missionsReducer = (state = initialState, action) => {
       return {
         ...state,
       };
-    case JOIN_LEAVE_MISSION: {
+    case TOGGLE_MISSION: {
       const s = state.data.map((e) => {
         if (e.missionId === action.payload) {
           return { ...e, status: !e.status };
