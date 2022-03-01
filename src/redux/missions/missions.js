38daca2fc637660077bfa46a0420590842637981
jihaneh <jijih@hotmail.fr>
2022-03-01 12:26:09 +0100

    Update Mission Reducer by adding a new action : INITIALIZE_MISSIONS

--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -1,33 +1,50 @@
 import { getMissionsFromAPI } from '../../api/spacexdata';
 
+const INITIALIZE_MISSIONS = 'space-travelers/missions/INITIALIZE_MISSIONS';
 const GET_MISSIONS = 'space-travelers/missions/GET_MISSIONS';
 const JOIN_LEAVE_MISSION = 'space-travelers/missions/JOIN_MISSION';
-const initialState = {};
 
-export const getMissions = (data) => ({
-  type: GET_MISSIONS,
+const initialState = {
+  isMissionsStored: false,
+};
+
+export const initializeMissions = (data) => ({
+  type: INITIALIZE_MISSIONS,
   payload: data,
 });
+
+export const initializeMissionsDispatcher = () => async (dispatch) => {
+  const missions = await getMissionsFromAPI();
+  dispatch(initializeMissions(missions));
+};
+
+export const getMissions = () => ({
+  type: GET_MISSIONS,
+});
+
+export const getMissionsDispatcher = () => async (dispatch) => {
+  dispatch(getMissions());
+};
 
 export const joinMission = (data) => ({
   type: JOIN_LEAVE_MISSION,
   payload: data,
 });
 
-export const getMissionsDispatcher = () => async (dispatch) => {
-  const missions = await getMissionsFromAPI();
-  dispatch(getMissions(missions));
-};
-
 export const joinMissionDispatcher = (id) => (dispatch) => dispatch(joinMission(id));
 export const leaveMissionDispatcher = (id) => (dispatch) => dispatch(joinMission(id));
 
 const missionsReducer = (state = initialState, action) => {
   switch (action.type) {
+    case INITIALIZE_MISSIONS:
+      return {
+        ...state,
+        isMissionsStored: true,
+        data: action.payload,
+      };
     case GET_MISSIONS:
       return {
         ...state,
-        data: action.payload,
       };
     case JOIN_LEAVE_MISSION: {
       const s = state.data.map((e) => {
@@ -37,6 +54,7 @@ const missionsReducer = (state = initialState, action) => {
         return e;
       });
       return {
+        ...state,
         data: s,
       };
     }
