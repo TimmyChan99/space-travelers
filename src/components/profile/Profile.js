8851f24310d38f16dcb6ee67bdd7da3d8aeb45ff
jihaneh <jijih@hotmail.fr>
2022-03-01 12:24:44 +0100

    Add active missons in Profile

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,5 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getMissionsDispatcher } from '../../redux/missions/missions';
 
 export default function Profile() {
-  return <div>My Profile</div>;
+  const dispatch = useDispatch();
+  const missionsDatas = useSelector((state) => state.missionsReducer);
+
+  useEffect(() => {
+    dispatch(getMissionsDispatcher());
+  }, []);
+
+  const missionsList = missionsDatas.data;
+  const missions = missionsList || [];
+
+  return (
+    <>
+      <div>My Profile</div>
+      <div>
+        <h2>My missions : </h2>
+        <div>
+          {missions
+            .filter((mission) => mission.status)
+            .map((filteredMission) => (
+              <li key={filteredMission.missionId} className="mission-name">
+                {filteredMission.missionName}
+              </li>
+            ))}
+        </div>
+      </div>
+    </>
+  );
 }
