17a9db50b2ee706e6476968fcf3c25ece584fa85
jihaneh <jijih@hotmail.fr>
2022-03-02 08:45:27 +0100

    remove call reducer in index.js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/configureStore';
 import App from './App';
-import { initializeMissionsDispatcher } from './redux/missions/missions';
-
-store.dispatch(initializeMissionsDispatcher());
 
 ReactDOM.render(
   <Provider store={store}>
